a4cfa1c8e78db6386de1831bf6ba08dd087d5776
James Ryan <jryan0004@gmail.com>
2018-04-06 19:03:21 -0400

    test

--- app/src/main/java/edu/temple/listenup/MainActivity.java
+++ app/src/main/java/edu/temple/listenup/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity implements SpotifyPlayer.Notification
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        System.out.println("jello");
     }
 
     @Override
