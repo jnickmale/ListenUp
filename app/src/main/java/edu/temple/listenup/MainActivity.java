a3f9ec13c9577be18a179700c2936abd1c00f607
James Ryan <jryan0004@gmail.com>
2018-04-06 17:51:12 -0400

    Added spotify login function, I also create the cliendt id , but i removed it from the project because i feel like that should not be on github and i will email to the group.

--- app/src/main/java/edu/temple/listenup/MainActivity.java
+++ app/src/main/java/edu/temple/listenup/MainActivity.java
@@ -3,11 +3,57 @@ package edu.temple.listenup;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+public class MainActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    //private  static final String REDIRECT_URI = "yourcustomprotocol://";
+
+    // TODO: Replace with your redirect URI
+    //private static final String REDIRECT_URI = "yourcustomprotocol://callback";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+
     }
 }
