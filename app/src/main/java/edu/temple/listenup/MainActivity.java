9a0433fa1385adbad79d39428082f7a690576741
James Ryan <jryan0004@gmail.com>
2018-04-07 01:38:30 -0400

    created a homescreen activity, add internent permission to the project

--- app/src/main/java/edu/temple/listenup/MainActivity.java
+++ app/src/main/java/edu/temple/listenup/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.listenup;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
 import com.spotify.sdk.android.player.PlayerEvent;
@@ -14,16 +17,19 @@ import retrofit.RestAdapter;
 
 public class MainActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
+    // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
+    private static final int REQUEST_CODE = 1337;
 
-    private  static final String Client_ID = "35c44a4ac64340ee951b71b2308ca072";
+    private  static final String CLIENT_ID = "35c44a4ac64340ee951b71b2308ca072";
 
     // TODO: Replace with your redirect URI
-    //private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+   // private static final String REDIRECT_URI = "yourcustomprotocol://callback";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         System.out.println("jello");
     }
