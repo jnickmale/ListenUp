5e3ad56344dbdfa9d0d24e89ad6889283bf28dae
James Ryan <jryan0004@gmail.com>
2018-04-08 19:40:49 -0400

    Add login with spotify, when a user logins in it plays a basic song, updated androidManifest.

--- app/src/main/java/edu/temple/listenup/MainActivity.java
+++ app/src/main/java/edu/temple/listenup/MainActivity.java
@@ -1,13 +1,24 @@
 package edu.temple.listenup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
@@ -17,55 +28,132 @@ import retrofit.RestAdapter;
 
 public class MainActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
+    private FirebaseAuth mAuth;
     // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
     private static final int REQUEST_CODE = 1337;
 
     private  static final String CLIENT_ID = "35c44a4ac64340ee951b71b2308ca072";
 
+    private Player mPlayer;
     // TODO: Replace with your redirect URI
-   // private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+    private static final String REDIRECT_URI = "testschema://callback";// test URI so spotify knows what app to send the info back to
 
+    private AuthenticationResponse response;
+
+ /*   @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+        }catch ( NullPointerException e)
+        {
+
+        }
+    }
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
 
-        System.out.println("jello");
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+
+
     }
 
     @Override
     public void onLoggedIn() {
+        Log.d("MainActivity", "User logged In");
+        // This is the line that plays a song.
+       /* mAuth = FirebaseAuth.getInstance();
 
+        mAuth.signInWithCustomToken(response.getAccessToken().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this,"it worked", Toast.LENGTH_LONG).show();
+
+                }else{
+                    Toast.makeText(MainActivity.this,"it didnt work :(", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        });
+*/
+        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
     }
 
     @Override
     public void onLoggedOut() {
-
+        Log.d("MainActivity", "User logged out");
     }
 
     @Override
     public void onLoginFailed(Error error) {
+        Log.d("MainActivity", "User login failed");
 
     }
 
     @Override
     public void onTemporaryError() {
 
+        Log.d("MainActivity", "temporary");
     }
 
     @Override
     public void onConnectionMessage(String s) {
-
+        Log.d("MainActivity", "connectionmessage");
     }
 
     @Override
     public void onPlaybackEvent(PlayerEvent playerEvent) {
-
+        Log.d("MainActivity", "playbackevnt");
     }
 
     @Override
     public void onPlaybackError(Error error) {
-
+        Log.d("MainActivity", "played back error");
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check if result comes from the correct activity
+        // The next 19 lines of the code are what you need to copy & paste! :)
+        if (requestCode == REQUEST_CODE) {
+             response = AuthenticationClient.getResponse(resultCode, data);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+
+
+        }
+    }//end onActivityResult
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+
 }
