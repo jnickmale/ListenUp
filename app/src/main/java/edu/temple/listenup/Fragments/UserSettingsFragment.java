35c78c5774f6be65b32e2f302bba6841a8756b36
GuillermoVazquez <wvvazque@gmail.com>
2018-04-10 13:46:46 -0400

    user settings form fragment

---
+++ app/src/main/java/edu/temple/listenup/Fragments/UserSettingsFragment.java
@@ -0,0 +1,29 @@
+package edu.temple.listenup.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.temple.listenup.R;
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UserSettingsFragment extends Fragment {
+
+
+    public UserSettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_settings, container, false);
+    }
+
+}
