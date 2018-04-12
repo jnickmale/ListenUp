de65806d5ad2264d8545748b29465492f9de5a34
James Ryan <jryan0004@gmail.com>
2018-04-12 01:23:19 -0400

    add user object

--- app/src/main/java/edu/temple/listenup/User.java
+++ app/src/main/java/edu/temple/listenup/User.java
@@ -8,4 +8,20 @@ public class User {
     private String spotify_access_Token, username,sex,email;
     double lon,lat, distance;
 
+    public User(String spotify_access_Token, String username, String sex, String email, double lon, double lat, double distance) {
+        this.spotify_access_Token = spotify_access_Token;
+        this.username = username;
+        this.sex = sex;
+        this.email = email;
+        this.lon = lon;
+        this.lat = lat;
+        this.distance = distance;
+    }
+
+    public User(String spotify_access_Token, String username, String sex, String email) {
+        this.spotify_access_Token = spotify_access_Token;
+        this.username = username;
+        this.sex = sex;
+        this.email = email;
+    }
 }
