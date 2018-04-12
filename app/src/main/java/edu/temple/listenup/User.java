e8b72baab5c938848ba80b960be86ae6d3905c57
James Ryan <jryan0004@gmail.com>
2018-04-12 17:00:00 -0400

    added push to the create user method, removed fragmentTransaction

--- app/src/main/java/edu/temple/listenup/User.java
+++ app/src/main/java/edu/temple/listenup/User.java
@@ -6,7 +6,11 @@ package edu.temple.listenup;
 
 public class User {
     private String spotify_access_Token, username,sex,email;
-    double lon,lat, distance;
+
+
+
+    private double lon,lat, distance;
+
 
     public User(String spotify_access_Token, String username, String sex, String email, double lon, double lat, double distance) {
         this.spotify_access_Token = spotify_access_Token;
@@ -24,4 +28,60 @@ public class User {
         this.sex = sex;
         this.email = email;
     }
+    public String getSpotify_access_Token() {
+        return spotify_access_Token;
+    }
+
+    public void setSpotify_access_Token(String spotify_access_Token) {
+        this.spotify_access_Token = spotify_access_Token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
 }
