f190ea3b0f9026545fb3a13050658260a976c1da
GuillermoVazquez <wvvazque@gmail.com>
2018-04-18 16:29:22 -0400

    check if users first time

--- app/src/main/java/edu/temple/listenup/HomeScreen.java
+++ app/src/main/java/edu/temple/listenup/HomeScreen.java
@@ -3,6 +3,7 @@ package edu.temple.listenup;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -20,6 +21,7 @@ import edu.temple.listenup.Fragments.UserSettingsFragment;
 public class HomeScreen extends AppCompatActivity {
 
     //the three fragments the user will navigate through
+    final String FIRST_TIME_USE = "first_time_check";
     Fragment userSettingsFragment = new UserSettingsFragment();
     Fragment chatList = new MatchesFragment();
     Fragment partnerList = new PartnerListFragment();
@@ -62,6 +64,18 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        navigation.setSelectedItemId(R.id.navigation_dashboard);
+        //check if this is the first time the user has gotten to the application
+        SharedPreferences settingsCheck = getSharedPreferences(FIRST_TIME_USE,0);
+        if( settingsCheck.getBoolean("user_first_time",true) ){
+            //user enters if it is the first time getting to this point of the application
+            //redirect user to settings tab and activate fab to set items
+            navigation.setSelectedItemId(R.id.navigation_home);
+            //activate the settings
+            //todo: check if this works!!!! :/
+            findViewById(R.id.profileFab).setActivated(true);
+            settingsCheck.edit().putBoolean("user_first_time",false);
+        }else{
+            navigation.setSelectedItemId(R.id.navigation_dashboard);
+        }
     }
 }
