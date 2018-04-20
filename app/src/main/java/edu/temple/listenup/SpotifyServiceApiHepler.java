e13864a3a4ba987074dd58a04bb5bc170e6f1107
James Ryan <jryan0004@gmail.com>
2018-04-20 17:54:34 -0400

    add spotifyServicehApiHelper for better organization

---
+++ app/src/main/java/edu/temple/listenup/SpotifyServiceApiHepler.java
@@ -0,0 +1,57 @@
+package edu.temple.listenup;
+
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+
+/**
+ * Created by kingJ on 4/20/2018.
+ */
+
+public class SpotifyServiceApiHepler implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    private Player mPlayer;
+    private String myAccessToken;
+
+    private SpotifyApi api;
+    private SpotifyService service;
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+
+    }
+}
