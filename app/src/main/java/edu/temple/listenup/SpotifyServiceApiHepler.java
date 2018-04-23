07036e6a28a6427592b7fb2d7c45bb67081637c5
James Ryan <jryan0004@gmail.com>
2018-04-23 18:18:36 -0400

    add spotifyServicehApiHelper for better organization

--- app/src/main/java/edu/temple/listenup/SpotifyServiceApiHepler.java
+++ app/src/main/java/edu/temple/listenup/SpotifyServiceApiHepler.java
@@ -1,5 +1,10 @@
 package edu.temple.listenup;
 
+import android.content.SharedPreferences;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Error;
 import com.spotify.sdk.android.player.Player;
@@ -15,11 +20,26 @@ import kaaes.spotify.webapi.android.SpotifyService;
 
 public class SpotifyServiceApiHepler implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
+
+
+    private FirebaseAuth mAuth;
+    // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
+    private static final int REQUEST_CODE = 1337;
+    private static final String CLIENT_ID = "35c44a4ac64340ee951b71b2308ca072";//you receive the client id from the developer dashbored
     private Player mPlayer;
     private String myAccessToken;
+    private DatabaseReference myDatabase;
 
     private SpotifyApi api;
     private SpotifyService service;
+    private SharedPreferences sharedPref;
+
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "listenup://callback";// test URI so spotify knows what app to send the info back to
+
+    private AuthenticationResponse response;
+
+    //test
     @Override
     public void onLoggedIn() {
 
