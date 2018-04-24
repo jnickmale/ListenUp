bbcbee54d1cb0068996e3bd571d86d28bf04442f
jnickmale <tug09183@temple.edu>
2018-04-23 20:22:59 -0400

    Base code for the ChatMessagesAdapter and itnegration with the fragment & activity

--- app/src/main/java/edu/temple/listenup/ChatMessagesAdapter.java
+++ app/src/main/java/edu/temple/listenup/ChatMessagesAdapter.java
@@ -1,17 +1,41 @@
 package edu.temple.listenup;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChatMessagesAdapter extends RecyclerView.Adapter {
+    private ArrayList messages;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public ChatMessagesAdapter(ArrayList messages){
+        this.messages = messages;
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_chat/*TODO replace with my layout for a MessageView*/, parent, false);
+
+        RecyclerView.ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        ((ViewHolder)holder).mTextView.setText(messages.get(position).toString());
     }
 
     @Override
--- app/src/main/java/edu/temple/listenup/Fragments/ChatFragment.java
+++ app/src/main/java/edu/temple/listenup/Fragments/ChatFragment.java
@@ -3,10 +3,15 @@ package edu.temple.listenup.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import edu.temple.listenup.ChatMessagesAdapter;
 import edu.temple.listenup.R;
 
 /**
@@ -14,17 +19,46 @@ import edu.temple.listenup.R;
  */
 public class ChatFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private ChatMessagesAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList myDataset;
+
 
     public ChatFragment() {
         // Required empty public constructor
     }
 
-    public static ChatFragment newInstance() {
+    public static ChatFragment newInstance(ArrayList messages) {
         ChatFragment fragment = new ChatFragment();
         Bundle args = new Bundle();
+        args.putSerializable("messages", messages);
         fragment.setArguments(args);
+
         return fragment;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setMyDataset((ArrayList)(getArguments().getSerializable("messages")));
+
+        mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.messagesView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter
+        mAdapter = new ChatMessagesAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,4 +67,7 @@ public class ChatFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_chat, container, false);
     }
 
+    public void setMyDataset(ArrayList myDataset) {
+        this.myDataset = myDataset;
+    }
 }
