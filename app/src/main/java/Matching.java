8b51b9d21acb7ace719ba48febfc367979cadbd3
GuillermoVazquez <wvvazque@gmail.com>
2018-04-26 16:45:06 -0400

    matching

---
+++ app/src/main/java/Matching.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.temple.listenup.User;
+
+/**
+ * Created by guillermo on 4/26/18.
+ */
+
+//this class wll assign each partner a compatibility score based on the user
+    //input: list of partners nearby
+    //output: list of partners WITH a compatibility score
+public class Matching {
+
+    List<User> list = new ArrayList<User>();
+
+    public List<User> getList() {
+        return list;
+    }
+
+
+}
