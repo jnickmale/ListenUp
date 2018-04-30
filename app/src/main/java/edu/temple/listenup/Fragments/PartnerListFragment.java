bef85d268ae3a7281ad6f5d9ea8a08ea0397cb53
James Ryan <jryan0004@gmail.com>
2018-04-29 23:13:12 -0400

    my new arm

--- app/src/main/java/edu/temple/listenup/Fragments/PartnerListFragment.java
+++ app/src/main/java/edu/temple/listenup/Fragments/PartnerListFragment.java
@@ -52,13 +52,13 @@ public class PartnerListFragment extends Fragment implements DatabaseHelper.Data
 
         //------- this is where we will insert the partner list data
         DatabaseHelper.getAllUsersWithinRadius(this, getActivity());
-
+/*
         List<String> list = new ArrayList<>();
         list.add("one");
         list.add("two");
         list.add("three");
         ///---------
-
+*/
         //connect data to adapter
         if (userList != null) {
             adapter = new PartnerListAdapter(userList, view.getContext());
